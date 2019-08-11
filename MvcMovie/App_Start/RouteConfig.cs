5ab4364442a4e8c259a918e911cf3993c9242132
g-esco101 <govinda.escobar@gmail.com>
2019-08-10 18:34:13 -0700

    Add files via upload

---
+++ MvcMovie/App_Start/RouteConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcMovie
+{
+    public class RouteConfig
+    {
+        /*The MapRoute method is used to route HTTP requests to the correct controller and action
+         * method and supply the optional ID parameter. The MapRoute method is also used by the 
+         * HtmlHelpers such as ActionLink to generate URLs given the controller, action method and 
+         * any route data.*/
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+            name: "Hello",
+            url: "{controller}/{action}/{name}/{id}"
+            );
+        }
+    }
+}
---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //The Index method above uses a view template to generate an HTML response to the browser.Controller methods
+        //(also known as action methods), such as the Index method above, generally return an ActionResult(or a 
+        //class derived from ActionResult), not primitive types like string.
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
